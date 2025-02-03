59e698f0816a0a60ea0ec6573b41ee0cfd374b18
Arivalagan Raghavan <80330613+ArivalaganRaghavan@users.noreply.github.com>
2025-02-03 11:02:03 +0530

    Update insurance_app_prediction.py
    minor correction in the code

--- insurance_app_prediction.py
+++ insurance_app_prediction.py
@@ -10,8 +10,13 @@ with open("insurance_premium_model_rfr.pkl", "rb") as f:
 ###with open("insurance_premium_model_lgbmr.pkl", "rb") as f1:
     ### model1 = pickle.load(f1)
 
-with open("insurance_premium_model_gbr.pkl", "rb") as f2:
-    model2 = pickle.load(f2)
+try:
+    with open('path_to_your_file.pkl', 'rb') as f2:
+        model2 = pickle.load(f2)
+except FileNotFoundError:
+    print("The file was not found.")
+except pickle.UnpicklingError:
+    print("The file is not a valid pickle file.")
 
 # define BMI caclulation
 def calculate_bmi(height, weight):
